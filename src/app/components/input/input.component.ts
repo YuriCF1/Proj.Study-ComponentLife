bb2565e4150fc4ced49540dd793f549975857e55
Yuri Cruz <yuricruzf@gmail.com>
2024-02-03 19:08:49 -0300

    Editing value

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { ItemInterface } from 'src/app/interfaces/iItem';
-import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
+import { listservice } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
@@ -9,14 +9,18 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 })
 export class InputComponent implements OnInit, OnChanges {
 
+  itemValue!: string;
   @Input() itemThatWillBeEdit!: ItemInterface
 
-  itemValue!: string;
+  editando: boolean = false;
+  textoBtn: string = 'Salvar item'
 
-  constructor(private service: ListaDeCompraService) { }
+  constructor(private listservice: listservice) { }
   ngOnChanges(changes: SimpleChanges): void {
-// 6 - CASO ELA MUDE, associa o valor do item a propriedade que veio do componente pai
-    if(!changes['itemThatWillBeEdit'].firstChange) { //Após o ponto, há diversos métodos a respeito do valor/situação da propriedade
+    // 6 - CASO ELA MUDE, associa o valor do item a propriedade que veio do componente pai
+    if (!changes['itemThatWillBeEdit'].firstChange) { //Após o ponto, há diversos métodos a respeito do valor/situação da propriedade
+      this.editando = true
+      this.textoBtn = 'Editar item'
       this.itemValue = this.itemThatWillBeEdit?.nome;
     }
   }
@@ -24,10 +28,17 @@ export class InputComponent implements OnInit, OnChanges {
   ngOnInit(): void { }
 
   adicionarItem() {
-    this.service.addItemIntoList(this.itemValue)
+    this.listservice.addItemIntoList(this.itemValue)
   }
 
   clearInput() {
     this.itemValue = ''
+  }
+
+  editarItem() {
+    this.listservice.editItemInList(this.itemThatWillBeEdit, this.itemValue)
+    this.clearInput()
+    this.editando = false;
+    this.textoBtn = 'Salvar item'
   }
 }
