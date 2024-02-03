00983b9d4ce7d2d31d232fc6ae6a2f47fa365f6e
Yuri Cruz <yuricruzf@gmail.com>
2024-02-02 23:29:44 -0300

    Using on changes

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { ItemInterface } from 'src/app/interfaces/iItem';
 import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
@@ -6,11 +7,19 @@ import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+
+  @Input() itemThatWillBeEdit!: ItemInterface
 
   itemValue!: string;
 
   constructor(private service: ListaDeCompraService) { }
+  ngOnChanges(changes: SimpleChanges): void {
+// 6 - CASO ELA MUDE, associa o valor do item a propriedade que veio do componente pai
+    if(!changes['itemThatWillBeEdit'].firstChange) { //Após o ponto, há diversos métodos a respeito do valor/situação da propriedade
+      this.itemValue = this.itemThatWillBeEdit?.nome;
+    }
+  }
 
   ngOnInit(): void { }
 
