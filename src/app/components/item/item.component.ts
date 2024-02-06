4ce47128a8a5b9d2ba95877101e4c85977102af8
Yuri Cruz <yuricruzf@gmail.com>
2024-02-05 22:46:31 -0300

    Adding OnDestroy

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ItemInterface } from 'src/app/interfaces/iItem';
 
@@ -7,7 +7,7 @@ import { ItemInterface } from 'src/app/interfaces/iItem';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit, OnChanges {
+export class ItemComponent implements OnInit, OnChanges, OnDestroy {
 
   faPen = faPen;
   faTrash = faTrash
@@ -38,7 +38,15 @@ export class ItemComponent implements OnInit, OnChanges {
   }
 
   deleteItem() {
-    console.log('Estao tentando me calar');
     this.emitindoIdParaDeletar.emit(this.item.id)
   }
+
+  ngOnDestroy() { //Chamado momentos antes de algo será retirado da tela
+    console.log('ngOnDestroy');
+  }
+
+  /*
+O método ngOnDestroy é um dos Lifecycle Hooks no Angular e é chamado quando um componente é destruído,
+ou seja, quando ele é removido do DOM. Este hook é útil para realizar tarefas de limpeza antes que o componente seja removido,
+como cancelar assinaturas de observáveis, desconectar eventos ou executar outras ações para evitar vazamento de memória ou problemas de desempenho.*/
 }
